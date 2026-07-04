1a176789ffc0c80d9d14c513372c5587b9ca0c80
hasan20101168 <mahmud99nsa@gmail.com>
2026-07-04 19:08:45 +0600

    socket io added on prod

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
-
+const http = require("http");
 const app = require("./app");
+const initializeSocket = require("./socket");
 
 const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+initializeSocket(server);
 
 // Local development only
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+server.listen(PORT, () => { 
+  console.log(`Server running on port ${PORT}`); 
+});
 
 module.exports = app;
+
+
